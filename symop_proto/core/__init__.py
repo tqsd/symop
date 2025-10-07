6d3614808c5a6617cf01045dce38e0bae1548f3d
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2025-10-07 15:32:08 +0200

    Core cleanup

---
+++ symop_proto/core/__init__.py
@@ -0,0 +1,12 @@
+from .monomial import Monomial
+from .operators import OperatorKind, ModeOp, LadderOp
+from .terms import KetTerm, DensityTerm
+
+__all__ = [
+    "Monomial",
+    "OperatorKind",
+    "ModeOp",
+    "LadderOp",
+    "KetTerm",
+    "DensityTerm",
+]
